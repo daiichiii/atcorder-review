09700601e1de8fe3033f6bfcb425b262c5cdf54a
daiichiii <dic20011006@gmail.com>
2024-07-05 07:28:34 +0900

    [ANS358] チケット購入し終わる時間を計算

---
+++ Atcoder_#358/b_ticket_counter.rb
@@ -0,0 +1,15 @@
+n, a = gets.chomp.split.map(&:to_i) 
+
+t = gets.chomp.split.map(&:to_i) 
+
+count_time = 0 
+
+# 各人がチケットを購入し終わる時間を計算
+n.times do |i|
+  if count_time <= t[i]
+    count_time = t[i] + a
+  else
+    count_time += a
+  end
+  puts count_time
+end
