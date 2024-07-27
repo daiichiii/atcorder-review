358de7e15dd0b50906011313e4d28b5f3e9d8511
daiichiii <136941425+daiichiii@users.noreply.github.com>
2024-07-28 08:15:59 +0900

    Update a_atcoder_line.js

--- Atcoder_#352/a_atcoder_line.js
+++ Atcoder_#352/a_atcoder_line.js
@@ -1,37 +1,42 @@
 const readline = require('readline');
 
-// 標準入力を読み取るためのインターフェースを作成
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-let inputLines = [];
-
-// 標準入力を読み取るイベントリスナー
-rl.on('line', (line) => {
-  inputLines.push(line);
-}).on('close', () => {
-  // 入力データを処理する関数を呼び出し
-  processInput(inputLines);
-});
-
-function processInput(input) {
-  const s = input[0].split('');
-  const t = input[1].split('');
-
-  let trueChars = [];
-  let lastChar = 0;
-
-  s.forEach((sChar) => {
-    for (let index = lastChar; index < t.length; index++) {
-      if (sChar === t[index]) {
-        trueChars.push(index + 1);
-        lastChar = index + 1;
-        break;
-      }
-    }
+function readInput(callback) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   });
 
-  console.log(trueChars.join(' '));
+  let inputLines = [];
+
+  rl.on('line', (line) => {
+    inputLines.push(line);
+  }).on('close', () => {
+    callback(inputLines);
+  });
 }
+
+function processInput(input) {
+  const [n, x, y, z] = input[0].split(' ').map(Number);
+
+  // 全駅を配列に変換
+  let stations = [];
+  for (let i = 1; i <= n; i++) {
+    stations.push(i);
+}
+
+
+  // x と y の順序を正規化
+  const [start, end] = [Math.min(x, y), Math.max(x, y)];
+
+  // 降りる駅を抽出
+  const getting_off_station = stations.slice(start - 1, end);
+
+  if (getting_off_station.includes(z)) {
+    console.log('Yes');
+  } else {
+    console.log('No');
+  }
+}
+
+// 入力を読み取り、コールバック関数で処理する
+readInput(processInput);
