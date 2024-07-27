ac7aa9afaa5dae5daa717d29f98da00df784bd91
daiichiii <dic20011006@gmail.com>
2024-07-27 22:15:33 +0900

    [ANS367]fix single quotation

--- Atcoder_#357/b_uppercase_and_lowercase.rb
+++ Atcoder_#357/b_uppercase_and_lowercase.rb
@@ -2,8 +2,8 @@
 S = gets.chomp
 
 # 大文字と小文字のカウント
-uppercase_count = S.count("A-Z")
-lowercase_count = S.count("a-z")
+uppercase_count = S.count('A-Z')
+lowercase_count = S.count('a-z')
 
 # 大文字の個数が小文字の個数より多い場合、小文字を大文字に変換
 if uppercase_count > lowercase_count
