0e2f2fa3c689dea94d38e5ba2e97500128e13c5f
daiichiii <dic20011006@gmail.com>
2024-08-14 07:44:17 +0900

    [ANS330]a_answer

--- Atcoder_#330/ruby/a_counting_passes.rb
+++ Atcoder_#330/ruby/a_counting_passes.rb
@@ -1,26 +1,10 @@
-function main(input) {
-    // 入力を改行で分割して配列にする
-    const lines = input.trim().split('\n');
+n,l = gets.split(" ").map(&:to_i)
+a = gets.split(" ").map(&:to_i)
 
-    // 最初の行の n, l を取得
-    const [n, l] = lines[0].split(' ').map(Number);
-
-    // 次の行の配列 a を取得
-    const a = lines[1].split(' ').map(Number);
-
-    // l 以上の数のカウントを行う
-    let count = 0;
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] >= l) {
-            count += 1;
-        }
-    }
-
-    // 結果を出力
-    console.log(count);
-}
-
-// 標準入力を読み込み、main関数を呼び出し
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin', 'utf8');
-main(input);
+count = 0
+a.each do |i|
+  if i >= l
+    count += 1
+  end
+end
+puts count
